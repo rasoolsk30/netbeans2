70db02d80d8903b24f36dfd79ee77d346a887be9
rasool <rasools27@gmail.com>
2018-09-19 07:50:33 +0530

    this project netbeans2

---
+++ src/test/java/PowerTest.java
@@ -0,0 +1,25 @@
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+public class PowerTest {
+    
+    public PowerTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+}
